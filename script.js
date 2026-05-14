d5ad9c640d017ad59a27dd78a5e0cc1d9a499ee0
zerocr54-code <zerocr54@gmail.com>
2026-05-14 22:28:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿// Eğer fotoğrafların uzantısı .png veya .jpeg ise burayı ona göre güncellemelisin.
+const images = [
+    'foto1.png',
+    'foto2.png',
+    'foto3.png',
+    'foto4.png'
+];
+
+let currentIndex = 0;
+const slider = document.getElementById('background-slider');
+
+function changeBackground() {
+    slider.style.backgroundImage = `url('${images[currentIndex]}')`;
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+// Sayfa ilk açıldığında birinci fotoğrafı hemen yükle
+changeBackground();
+
+// 5000 milisaniye (5 saniye) aralıklarla fotoğrafı değiştir
+setInterval(changeBackground, 5000);
