34653d3cca9d5a0376d88ba191840d7adf4a4720
zerocr54-code <zerocr54@gmail.com>
2026-05-15 00:23:01 +0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     const password = document.getElementById('password-input').value;
-    const correctPassword = "1550"; // Buraya istediğin şifreyi yaz!
+    const correctPassword = "1550";
 
     if (password === correctPassword) {
         // Şifre doğruysa: Giriş ekranını sil, ana içeriği göster ve müziği başlat
